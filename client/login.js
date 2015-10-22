49de4bb7b730c37cef5767d44503bb3dca5dcec5
dmartines <dbmartines@gmail.com>
2015-10-22 15:19:38 -0400

    Push to meteor.com

--- client/login.js
+++ client/login.js
@@ -1,3 +1,32 @@
+Template.login.onRendered(function() {
+
+    console.log("Current user");
+
+    window.fbAsyncInit = function() {
+
+        FB.init({
+            appId      : '510246152469337',
+            xfbml      : true,
+            version    : 'v2.5'
+        });
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+
+    };
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+});
+
+
+
 Template.login.events({
     'click #facebook-login': function(event) {
         Meteor.loginWithFacebook({}, function(err){
@@ -15,3 +44,28 @@ Template.login.events({
         })
     }
 });
+
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+        FB.api('/me', {fields: 'last_name'}, function(response) {
+            console.log(response);
+        });
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
