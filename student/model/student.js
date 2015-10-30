b1c62c008d4b1bf7bec9fe25af9a94b96644a957
dmartines <dbmartines@gmail.com>
2015-10-30 08:32:45 -0400

    Testing collection hooks

--- student/model/student.js
+++ student/model/student.js
@@ -18,32 +18,25 @@ Student.allow({
 	}
 });
 
-if(Meteor.isClient){
+Student.before.insert(function(userId, doc) {
+	doc.createdOn = new Date();
+	doc.userId = userId;
+});
 
-	if (Student.before) {
-		Student.before.insert(function(userId, doc) {
-			doc.createdOn = new Date();
-			doc.userId = userId;
-		});
+Student.before.update(function(userId, doc, fieldNames, modifier, options) {
+	modifier.$set = modifier.$set || {};
+	modifier.$set.updateOn = Date.now();
+	modifier.$set.updatedUserId = userId;
+});
 
-		Student.before.update(function(userId, doc, fieldNames, modifier, options) {
-			modifier.$set = modifier.$set || {};
-			modifier.$set.updateOn = Date.now();
-			modifier.$set.updatedUserId = userId;
-		});
-	}
+Student.after.insert(function(userId, doc) {
+	var successMessage = "Job " + doc.name + " was created successfully";
+	$('#successMessage').text(successMessage);
+	$('#successAlert').slideDown();
+});
 
-	if (Student.after) {
-		Student.after.insert(function(userId, doc) {
-			var successMessage = "Job " + doc.name + " was created successfully";
-			$('#successMessage').text(successMessage);
-			$('#successAlert').slideDown();
-		});
-
-		Student.after.update(function(userId, doc, fieldNames, modifier, options) {
-			var successMessage = "Organization " + doc.name + " was updated successfully";
-			$('#successMessage').text(successMessage);
-			$('#successAlert').slideDown();
-		});
-	}
-}
+Student.after.update(function(userId, doc, fieldNames, modifier, options) {
+	var successMessage = "Organization " + doc.name + " was updated successfully";
+	$('#successMessage').text(successMessage);
+	$('#successAlert').slideDown();
+});
